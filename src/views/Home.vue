9cf63f4b05433a3d61d2dcd8a799b30dc2559e97
Antonio <tonioufa@gmail.com>
2023-02-20 15:43:50 +0000

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="text-center pt-12">
+    <h1 class="text-2xl mb-8">This is the Home page</h1>
+    <p v-if="walletStore.address != ''">
+      Your wallet address is: {{ walletStore.acc_short }}
+    </p>
+    <p v-else>You've not connected your wallet yet</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+import { useWalletStore } from '../stores/wallet'
+
+export default defineComponent({
+  setup() {
+    const walletStore = useWalletStore()
+
+    return {
+      walletStore,
+    }
+  },
+})
+</script>
